5d58188a7d7aca0530f38985869a3d93d5ecb5b8
John Lu <lush93@outlook.com>
2024-01-20 11:24:52 +0800

    feat: Upgrade to python3

--- arraysAndStrings/E217.py
+++ arraysAndStrings/E217.py
@@ -1,5 +1,5 @@
 class E217(object):
-    def containsDuplicate(self, nums):
+    def containsDuplicate(self, nums: List[int]) -> bool:
         set1 = set()
         for num in nums:
             if num in set:
