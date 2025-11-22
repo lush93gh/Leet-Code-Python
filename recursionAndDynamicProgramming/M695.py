d336dfbf63f75330f8ef8167e8b36a8108bb170e
John Lu <lush93@outlook.com>
2025-11-22 17:37:47 +0800

    feat: refactor maxAreaOfIsland method to use BFS and add DFS implementation for area calculation

--- recursionAndDynamicProgramming/M695.py
+++ recursionAndDynamicProgramming/M695.py
@@ -2,7 +2,7 @@ from typing import List
 import queue
 
 class M695:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+    def maxAreaOfIslandBFS(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
         visited = set()
@@ -39,4 +39,35 @@ class M695:
                             current_area += 1
                     if current_area > max_area:
                         max_area = current_area
+        return max_area
+    
+    def dfs(self, i: int, j: int) -> int:
+        m = len(self.grid)
+        n = len(self.grid[0])
+        if i >= 0 and i < m and j >=0 and j < n and self.grid[i][j] == 1 and (i, j) not in self.visited:
+            self.visited.add((i, j))
+            up = j -1
+            down = j + 1
+            right = i -1
+            left = i + 1
+            return 1 + self.dfs(i, up) + self.dfs(i, down) + self.dfs(left, j) + self.dfs(right, j)
+        else:
+            return 0
+
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        m = len(grid)
+        n = len(grid[0])
+        self.visited = set()
+
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                current_area = 0
+                if grid[i][j] == 1 and (i, j) not in self.visited:
+                    current_area = self.dfs(i, j)
+                    if current_area > max_area:
+                        max_area = current_area
+
         return max_area
